dcc3e783f285604362719feb469e792469a865a0
Ash Khan <63376512+AshKhanNY@users.noreply.github.com>
2021-02-28 23:20:49 -0500

    Add files via upload

---
+++ Homework0.scm
@@ -0,0 +1,19 @@
+; Homework 0
+; Spring 2021
+
+
+; You should begin working on the induction and logic review problems I posted -- make a point of having worked on these
+; before we need that material in class.  You will want to review weak, strong and structural induction - these topics
+; covered in almost all Discrete Math I texts.
+
+; You should also take this quiet time at the start of the course to familiarize yourself with Scheme, using DrRacket.
+; This wonderful package includes a great deal of documentation -- including the full manual for R5RS Scheme.  I hope
+; you notice - and enjoy - the fact that this manual is very short.  It is the sort of thing that might go well with
+; your Cheerios in the morning.
+
+; I take it for granted - I hope I may take it for granted! - that students at your level come to lectures having
+; studied previous lectures, to the point where either you understand them and have no questions, or you have specific
+; questions that can be addressed in class or the office hour.
+
+
+
