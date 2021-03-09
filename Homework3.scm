6da0d40edf9fae7ababe379b724f410319376d1c
Ash Khan <63376512+AshKhanNY@users.noreply.github.com>
2021-03-08 23:03:53 -0500

    Add files via upload

---
+++ Homework3.scm
@@ -0,0 +1,52 @@
+
+; Third Homework Set
+; CSc 335
+; Spring 2021
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; based on lecture5.scm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Review the lecture notes, and read the associated
+; sections in Abelson and Sussman, before starting
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Proofs are required for all programs
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; This is another long problem set
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+; Abelson and Sussman, Problems 1.29, 1.30, 1.31, 1.34, and 1.37
+
+; Abelson and Sussman, Problems 1.41, 1.42, and 1.43
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Exercise 1.29
+
+; Exercise 1.30
+
+; Exercise 1.31
+
+; Exercise 1.34
+
+; Exercise 1.37
+
+
+
+; Exercise 1.41
+
+; Exercise 1.42
+
+; Exercise 1.43
+
